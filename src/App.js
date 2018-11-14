9cb66e1329290e001f1cb8abe437e8044726597e
rdalf2 <rdalf2@student.monash.edu>
2018-11-15 01:01:51 +1100

    Add CI tests and some javascript files for UI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import {Button,Grid, Row, Col} from 'react-bootstrap'; // Button from Bootstrap
 import './App.css';
 
+const MediaQuery = require('react-responsive');
+
+const alignment = {
+    grid: {
+        paddingLeft: 100,
+        paddingRight: 100
+    },
+    row: {
+        marginLeft: 0,
+        marginRight: 0
+    },
+    col: {
+        paddingLeft: 0,
+        paddingRight: 0
+    }
+};
 class App extends Component {
   render() {
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <ItIsMe/>
+          <Grid fluid style={alignment.grid}>
+              <Row style={alignment.row}>
+                  <Col style={alignment.col}>
+                      <Button bsSize="Large " bsStyle="warning" className="btn-gold">SEND</Button>
+                  </Col>
+              </Row>
+          </Grid>
       </div>
     );
   }
 }
+//Header
+const MyHeader = () =>
+    <StickyContainer >
+        <Sticky>
+            {
+
+            }
+        </Sticky>
+    </StickyContainer>
+
+
+//Introduction
+const ItIsMe = () =>
+        <div className={"myName"}>
+            <h1>RIORDAN</h1>
+            <h1>DERVIN</h1>
+            <h1>ALFREDO</h1>
+            <p>freelance full-stack developer</p>
+        </div>
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,4 +5,7 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+if(module.hot){
+    module.hot.accept();
+}
 registerServiceWorker();

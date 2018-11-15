cb0695b0b4aee41aafb89f1d6ce0023ea7f21604
rdalf2 <rdalf2@student.monash.edu>
2018-11-15 18:33:34 +1100

    Finish introduction part

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './img/logo.svg';
+import blank from './img/blank.png';
+import profFoto from './img/portrait.jpg';
 import {Button,Grid, Row, Col} from 'react-bootstrap'; // Button from Bootstrap
 import MediaQuery from 'react-responsive';
 
@@ -8,12 +10,22 @@ import './App.css';
 
 
 class App extends Component {
-  render() {
+    constructor(props)
+    {
+        super(props);
 
+        this.state = {
+            hyperlink:'',
+        };
+    }
+
+  render() {
+    const {hyperlink} = this.state;
     return (
       <div className="App">
-          <MediaQuery minWidth={1224} >
-              <Grid>
+          <MediaQuery minWidth={990} maxWidth={1400}>
+              <CustomHeader/>
+              <Grid className={"introduction"} id={"introduction"}>
                   <Row className="show-grid">
                       <Col md={8} >
                           <ItIsMe/>
@@ -23,14 +35,54 @@ class App extends Component {
                       </Col>
                   </Row>
               </Grid>
+              <Projects/>
+              <Skills/>
+          </MediaQuery>
+          <MediaQuery maxWidth={990}>
 
           </MediaQuery>
+          <Footer/>
       </div>
     );
   }
 }
 //Header
 
+const CustomHeader = () =>
+    <div className="header" id={"navbar"} >
+        <Grid>
+            <Row>
+                <Col md={2}>
+                    <img src={blank} width={110}/>
+                </Col>
+                <Col md={10} className={"text-right"}>
+                    <AnchorLink name={"HOME"} link={"#introduction"} />
+                    <AnchorLink name={"PROJECTS"} link={"#projects"} />
+                </Col>
+            </Row>
+        </Grid>
+        <div className="progress-container">
+            <div className="progress-bar" id="progressBar"></div>
+        </div>
+    </div>
+
+//Scrolling progress bar
+window.onscroll = function() {scrolling()};
+function scrolling() {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
+
+//Link Header Anchor
+const AnchorLink = ({name,link}) =>
+        <a href={link}>
+            <h3 className={"anchorLink"}>
+                {name}
+            </h3>
+        </a>
 
 //Introduction
 const ItIsMe = () =>
@@ -43,7 +95,26 @@ const ItIsMe = () =>
 
 const Design = () =>
         <div className={"picture"}>
-            <img src={logo}/>
+            <img src={profFoto} width={350}/>
         </div>
+
+//Projects
+const Projects = () =>
+    <div className={"projects"} id={"projects"}>
+        Noice
+    </div>
+
+//Skills
+const Skills = () =>
+    <div>
+        test again
+    </div>
+
+const Footer = () =>
+    <footer>
+        Developed with React<img src={logo} width={30}/>
+        by <b> Riordan Dervin Alfredo </b>&copy; 2018
+    </footer>
+
 
 export default App;

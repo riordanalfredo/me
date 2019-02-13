49efb86163db9b0601202f0c121740713ad6b0a1
rdalf2 <rdalf2@student.monash.edu>
2019-02-13 15:25:22 +1100

    Updates

---
+++ precache-manifest.7c455429c5eaec2832da53da7c0ca93b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfeb341dc253c19c34ef28b0a21c577f",
+    "url": "/static/media/resume.dfeb341d.pdf"
+  },
+  {
+    "revision": "aadedec7c0551578dc3cf8b92af20db7",
+    "url": "/static/media/portrait.aadedec7.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "3259ebad2b7ed4be0e37139f34a74140",
+    "url": "/static/media/blank.3259ebad.png"
+  },
+  {
+    "revision": "6f3c76ba45e011fa1221957decb73bf7",
+    "url": "/static/media/aus.6f3c76ba.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "caf6e5d58a49c3a6114d",
+    "url": "/static/js/main.caf6e5d5.chunk.js"
+  },
+  {
+    "revision": "4e7482cb980488933a56",
+    "url": "/static/js/1.4e7482cb.chunk.js"
+  },
+  {
+    "revision": "caf6e5d58a49c3a6114d",
+    "url": "/static/css/main.c34809cc.chunk.css"
+  },
+  {
+    "revision": "e3e045dc750ac6f663d3ceee954e423c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "riordanalfredo.github.io/me/precache-manifest.a201dae9b5792a146913ff9d350b6e57.js"
+  "/precache-manifest.7c455429c5eaec2832da53da7c0ca93b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("riordanalfredo.github.io/me/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

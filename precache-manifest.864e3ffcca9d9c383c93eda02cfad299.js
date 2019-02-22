fb3fd650e01ef7f08d416d3efb138940a959a421
Riordan <riordan.alfredo@gmail.com>
2019-02-22 20:25:31 +1100

    Updates

---
+++ precache-manifest.864e3ffcca9d9c383c93eda02cfad299.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dfab1071b1ecd27edd5",
+    "url": "/static/css/main.8268f598.chunk.css"
+  },
+  {
+    "revision": "0dfab1071b1ecd27edd5",
+    "url": "/static/js/main.0dfab107.chunk.js"
+  },
+  {
+    "revision": "7a035110a3414163b08f",
+    "url": "/static/js/1.7a035110.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "3259ebad2b7ed4be0e37139f34a74140",
+    "url": "/static/media/blank.3259ebad.png"
+  },
+  {
+    "revision": "6f3c76ba45e011fa1221957decb73bf7",
+    "url": "/static/media/aus.6f3c76ba.png"
+  },
+  {
+    "revision": "aadedec7c0551578dc3cf8b92af20db7",
+    "url": "/static/media/portrait.aadedec7.jpg"
+  },
+  {
+    "revision": "3a7e30fb9d6e2e72bfea370d5fef6dcf",
+    "url": "/static/media/resume.3a7e30fb.pdf"
+  },
+  {
+    "revision": "4a2d5ff556aaf4a35f7d4ac0c8fcd7a4",
+    "url": "/index.html"
+  }
+];

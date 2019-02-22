1e05ffdaa9e888d855c1f2c1de9aafadbbdbe545
Riordan <riordan.alfredo@gmail.com>
2019-02-22 20:16:25 +1100

    Updates

---
+++ precache-manifest.e0f0c7f62e4f68a47290fbc4275bb101.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fd568ee73b1a7d67844",
+    "url": "rioalfredo.com/static/css/main.67d4d776.chunk.css"
+  },
+  {
+    "revision": "6fd568ee73b1a7d67844",
+    "url": "rioalfredo.com/static/js/main.6fd568ee.chunk.js"
+  },
+  {
+    "revision": "7a035110a3414163b08f",
+    "url": "rioalfredo.com/static/js/1.7a035110.chunk.js"
+  },
+  {
+    "revision": "03fdfbcac96c489e652c",
+    "url": "rioalfredo.com/static/js/runtime~main.03fdfbca.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "rioalfredo.com/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "3259ebad2b7ed4be0e37139f34a74140",
+    "url": "rioalfredo.com/static/media/blank.3259ebad.png"
+  },
+  {
+    "revision": "6f3c76ba45e011fa1221957decb73bf7",
+    "url": "rioalfredo.com/static/media/aus.6f3c76ba.png"
+  },
+  {
+    "revision": "aadedec7c0551578dc3cf8b92af20db7",
+    "url": "rioalfredo.com/static/media/portrait.aadedec7.jpg"
+  },
+  {
+    "revision": "3a7e30fb9d6e2e72bfea370d5fef6dcf",
+    "url": "rioalfredo.com/static/media/resume.3a7e30fb.pdf"
+  },
+  {
+    "revision": "f01d2a58a7246756a72db51d34cc20f9",
+    "url": "rioalfredo.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.864e3ffcca9d9c383c93eda02cfad299.js"
+  "rioalfredo.com/precache-manifest.e0f0c7f62e4f68a47290fbc4275bb101.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("rioalfredo.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
